6727a5407e0697d2878b6dcd3b3335c2b213dde4
steakpick <zalucan.dima@gmail.com>
2021-12-19 14:43:45 +0200

    Fixed board and routing on the page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './App.css';
-import React, {useState} from "react";
+import React from "react";
 import NavbarComponent from "./componets/navbar/NavbarComponent";
 import Tasks from "./componets/pages/tasks/Tasks";
-import Board from "./componets/board/Board";
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
+import Home from "./componets/pages/home/Home";
 
 
 function App() {
@@ -14,8 +14,8 @@ function App() {
             <NavbarComponent/>
             <Routes>
                 <Route path='/tasks' element={<Tasks/>}/>
+                <Route path='/' element={<Home/>}/>
             </Routes>
-            <Board />
         </div>
 
 
---
+++ src/componets/pages/home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Board from "../../board/Board";
+
+function Home() {
+    return (
+        <div className='home'>
+            <Board/>
+        </div>
+    );
+}
+
+export default Home;
